17d50416a761e51589c420b63a33ca1968762152
Breogan <breogangg@gmail.com>
2025-01-28 18:27:58 +0100

    first commit pero no porque soy subnormal

---
+++ src/marcoController.java
@@ -0,0 +1,34 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.media.MediaView;
+
+public class marcoController {
+
+    @FXML
+    private Button btnPlay;
+
+    @FXML
+    private Label lblDuration;
+
+    @FXML
+    private MediaView mediaView;
+
+    @FXML
+    void btnPlay(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btnStop(MouseEvent event) {
+
+    }
+
+    @FXML
+    void selectMedia(ActionEvent event) {
+
+    }
+
+}
